376131c052b3f743037bd982e69eced84e9fcab2
Kuzey Kartal <kkturkog@upv.edu.es>
2025-10-01 12:40:20 +0200

    practica1: hola en C++

---
+++ practica1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola,este es mi primer programa en C++!" << endl;
+    cout << "funciona correctamente" << endl; 
+    return 0;
+}
